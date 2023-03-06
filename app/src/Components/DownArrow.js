77036a0a88461b61c67aab596fcc94be8a82c60f
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-06 20:09:27 +0000

    converted some stuff to react

---
+++ app/src/Components/DownArrow.js
@@ -0,0 +1,17 @@
+function DownArrow() {
+    return (
+        <>
+        <section>
+            <a href="#about">
+                <ul id="downArrow">
+                    <li style={"--i:1"}></li>
+                    <li style={"--i:2"}></li>
+                    <li style={"--i:3"}></li>
+                </ul>
+            </a>
+        </section>
+        </>
+    )
+}
+
+export default DownArrow;
---
+++ app/src/Components/Main Container/ProfileWrapper.js
@@ -0,0 +1,12 @@
+import ProfileInfo from "./ProfileInfo";
+
+function ProfileWrapper() {
+    return (
+        <>
+        <div className="wrapper">
+            <ProfileInfo />
+        </div>
+        </>
+    )
+}
+export default ProfileWrapper;
