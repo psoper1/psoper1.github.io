48105ef8191d31f03324029758a440e543ad8486
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-07 17:23:50 +0000

    got social icons working

--- app/src/Components/Main Container/SocialIcons.js
+++ app/src/Components/Main Container/SocialIcons.js
@@ -1,10 +1,12 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 function SocialIcons() {
     return (
         <>
          <ul className="social-icons">
-                    <ul><a href="https://www.linkedin.com/in/preston-soper-782586100/"><i className="fa fa-lg fa fa-linkedin"></i></a></ul>
-                    <ul><a href="https://github.com/psoper1/"><i className="fa fa-lg fa-github"></i></a></ul>
-                    <ul><a href=""><i className="fa fa-lg fa-phone"></i></a>
+                    <ul><a href="https://www.linkedin.com/in/preston-soper-782586100/"><i className="bi bi-linkedin"></i></a></ul>
+                    <ul><a href="https://github.com/psoper1/"><i className="bi bi-github"></i></a></ul>
+                    <ul><a href=""><i className="bi bi-telephone-fill"></i></a>
                     </ul>
                     </ul>
         </>
