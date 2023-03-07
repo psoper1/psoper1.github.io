18b0187a37f822a973fa481177b3e0f8b4224717
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-07 20:06:39 +0000

    trying to make the icons responsive

--- app/src/Components/Main Container/SocialIcons.js
+++ app/src/Components/Main Container/SocialIcons.js
@@ -4,10 +4,10 @@ function SocialIcons() {
     return (
         <>
          <ul className="social-icons">
-                    <ul><a href="https://www.linkedin.com/in/preston-soper-782586100/"><i className="bi bi-linkedin"></i></a></ul>
-                    <ul><a href="https://github.com/psoper1/"><i className="bi bi-github"></i></a></ul>
-                    <ul><a href=""><i className="bi bi-telephone-fill"></i></a>
-                    </ul>
+                    <li><a href="https://www.linkedin.com/in/preston-soper-782586100/"><i className="bi bi-linkedin"></i></a></li>
+                    <li><a href="https://github.com/psoper1/"><i className="bi bi-github"></i></a></li>
+                    <li><a href=""><i className="bi bi-telephone-fill"></i></a>
+                    </li>
                     </ul>
         </>
     )
