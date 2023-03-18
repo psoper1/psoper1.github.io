29d96ec31f560f1963578337c180b57a1c09373b
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-18 17:53:21 +0000

    added routes, added about me and added footer

--- app/src/App.js
+++ app/src/App.js
@@ -2,19 +2,16 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Components/Home';
 import Blog from './Components/Blog';
-import Footer from './Components/Footer';
 
 function App() {
   return (
     <>
-    <Router>
-
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blog" element={<Blog />} />
-    </Routes>
-    </Router>
-    
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- app/src/Components/Footer.js
+++ app/src/Components/Footer.js
@@ -1,23 +1,13 @@
 function Footer() {
     return (
-        <>
-            
+        <>           
             <div className="container my-5">
-
                 <footer className="bg-dark text-center text-white">
-                    
-                    
-                    
-
-                    
                     <div className="footer text-center p-3">
                         © 2023 Copyright: Potato Man
                     </div>
-                    
                 </footer>
-
-            </div>
-            
+            </div>           
         </>
     )
 }
