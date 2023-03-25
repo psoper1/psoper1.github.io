9d1c2923d8f099ae51746ce1a958f22e23a3d0a2
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-25 19:15:49 +0000

    trying to fix links after deployment

--- app/src/Components/AboutMe.js
+++ app/src/Components/AboutMe.js
@@ -2,7 +2,7 @@ function AboutMe() {
     return (
         <>
             <section className="text-center">
-                <h3 id="about">Hello! My name is Preston. Coding is my jam! Also gaming, building PC's, writing and playing music, telling jokes and well, jam... is also my jam!</h3>
+                <h3 id="about">TEST Hello! My name is Preston. Coding is my jam! Also gaming, building PC's, writing and playing music, telling jokes and well, jam... is also my jam!</h3>
             </section>
         </>
     )
