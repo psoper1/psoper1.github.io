685d550827eae73509ff4e4adccd052c45909746
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-25 17:51:48 +0000

    added projects to project page

---
+++ app/src/Components/Projects.js
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+import ProjectsCode from "./ProjectsCode";
+
+function Projects() {
+    return (
+        <>
+        <NavBar />
+        <ProjectsCode />
+        </>
+    )
+}
+
+export default Projects;
