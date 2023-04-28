e4150d506753e48e38b69a7122237efcc678e98b
reeman9000 <preston.soper@student.awesomeinc.org>
2023-04-28 14:10:07 +0000

    fixed social icon layout for the most part

--- app/src/Components/AboutMe.js
+++ app/src/Components/AboutMe.js
@@ -1,9 +1,9 @@
 function AboutMe() {
     return (
         <>
-            <section className="text-center">
+            <div className="text-center">
                 <h3 id="about">Hello! My name is Preston. Coding is my jam! Also gaming, building PC's, writing and playing music, telling jokes and well, jam... is also my jam!</h3>
-            </section>
+            </div>
         </>
     )
 }
